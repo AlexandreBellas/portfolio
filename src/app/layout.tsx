e59a21be167cc8d35235ca53397d5b2207893b66
Alexandre Batistella Bellas <alexandre.bellas@gmail.com>
2024-05-30 13:20:57 -0300

    feat: add metadata to the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,25 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'Alexandre Batistella | Portfolio',
   description: 'Get to know better Alexandre Batistella',
+  keywords: [
+    'portfolio',
+    'nextjs',
+    'alebatistella',
+    'github',
+    'fullstack',
+    'software',
+    'engineer',
+    'react',
+    'reactjs',
+    'node',
+    'nodejs',
+  ],
   icons:
     "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>👤</text></svg>",
+  openGraph: {
+    title: 'Alexandre Batistella | Portfolio',
+    description: 'Get to know better Alexandre Batistella',
+  },
 }
 
 export default function RootLayout({
